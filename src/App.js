56e4ed5577738aa9a1de4a9ef1778833b309ac72
ralphxp <91854095+ralphxp@users.noreply.github.com>
2021-12-03 10:29:36 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 import Home from './components/home';
 
-
+import Nav from './components/nav';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <>
-      
-    </>
+    <Router>
+      <Nav />
+
+      <Footer />
+    </Router>
   );
 }
 
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,10 +1,36 @@
-
+import {Link} from 'react-router-dom';
+import $ from 'jquery';
 
 
 function Nav()
 {
+	function toggle(e) {
+    document.querySelector('#navbar').classList.toggle('navbar-mobile')
+    e.target.classList.toggle('bi-list')
+    e.target.classList.toggle('bi-x')
+  }
+
 	return(
-		
+		<header id="header" className="fixed-top">
+		    <div className="container d-flex align-items-center justify-content-between">
+
+		      <h1 className="logo"><Link to="/">tinyPay</Link></h1>
+		     
+
+		      <nav id="navbar" className="navbar">
+		        <ul>
+		          <li><Link className="nav-link scrollto active" to="/">Home</Link></li>
+		          {/*<li><Link className="nav-link scrollto" to="#about">About</Link></li>*/}
+		          <li><Link to="#">Pay Money</Link></li>
+		          <li><Link to="#">Verify Payment</Link></li>
+		          <li><Link to="#">Request Receipt</Link></li>
+		          <li><Link to="#">Drop a Complain</Link></li>
+		        </ul>
+		        <i className="bi bi-list mobile-nav-toggle" onClick={toggle}></i>
+		      </nav>
+
+		    </div>
+  </header>
     );
 }
 
